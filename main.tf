563d8da6f0e18ee3041689fb3621ab9e79297fc0
LucasDiMare <124387887+LucasDiMare@users.noreply.github.com>
2023-07-19 19:06:37 -0300

    Create main.tf
    main.tf

---
+++ main.tf
@@ -0,0 +1,43 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = ">= 2.15.0"
+    }
+  }
+}
+
+provider "docker" {
+  host = "npipe:////./pipe/docker_engine"
+}
+
+# Criação do container do banco de dados MariaDB
+resource "docker_container" "database" {
+  name    = "laravel-db"
+  image   = "mariadb"
+  restart = "unless-stopped"
+  ports {
+    internal = 3306
+    external = 3306
+  }
+  env = [
+    "MYSQL_ROOT_PASSWORD=root"
+  ]
+}
+
+# Criação do container do servidor web Apache
+resource "docker_container" "webserver" {
+  name    = "laravel-web"
+  image   = "php:7.4-apache"
+  restart = "unless-stopped"
+  ports {
+    internal = 80
+    external = 8080
+  }
+  volumes {
+    container_path = "/var/www/html"
+    host_path      = "C:\\Users\\caminho\\do\\projeto\\laravel" # Substitua pelo caminho absoluto correto
+    read_only      = false
+  }
+  depends_on = [docker_container.database]
+}
